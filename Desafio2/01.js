232727e272041de142eb8d4ab4493ddb58f7250a
Jose Cassios <cassios.torres.010@gmail.com>
2024-05-03 15:32:33 -0300

    refactor: use local scope variables in Operacao function

--- Desafio2/01.js
+++ Desafio2/01.js
@@ -1,14 +1,14 @@
 function Operacao(num1, num2){
-    soma_exponenciacao = num1**2 + num2**2;
-    resto = num1 % num2;
-    subtracao = num2 - num1;
+    let soma_exponenciacao = num1**2 + num2**2;
+    let resto = num1 % num2;
+    let subtracao = num2 - num1;
 
     alert(`A soma da exponenciação (ao quadrado) é: ${soma_exponenciacao}`);
     alert(`O resto da divisão de ${num1} por ${num2} é: ${resto}`);
     alert(`A subtração de ${num2} por ${num1} é: ${subtracao}`);
 }
 
-num1 = parseInt(prompt("Digite o 1° valor: "));
-num2 = parseInt(prompt("Digite o 2° valor: "));
+let num1 = parseInt(prompt("Digite o 1° valor: "));
+let num2 = parseInt(prompt("Digite o 2° valor: "));
 
 Operacao(num1, num2);
